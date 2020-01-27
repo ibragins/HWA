047a4a50c301d7ef07701ee3e3894bc05a574c35
Igor Braginsky <ibragins@redhat.com>
2020-01-27 15:15:42 +0200

    Moved resource definitions to class

--- LinuxHost.py
+++ LinuxHost.py
@@ -1,7 +1,12 @@
+import multiprocessing
+from psutil import virtual_memory
+mem = virtual_memory()
+
+
 class LinuxHost:
-    def __init__(self, ram, cores):
-        self.total_ram = ram
-        self.total_cores = cores
+    def __init__(self):
+        self.total_ram = mem.total / 1024 / 1024
+        self.total_cores = multiprocessing.cpu_count()
 
     def __str__(self):
         return "Total RAM:\t" + str(self.total_ram) + "\nCores:\t\t" + str(self.total_cores) + "\n"
--- RunHWA.py
+++ RunHWA.py
@@ -1,11 +1,6 @@
-from psutil import virtual_memory
-import multiprocessing
 from LinuxHost import LinuxHost
 
-mem = virtual_memory()
-totalMemory = mem.total / 1024 / 1024
-totalCores = multiprocessing.cpu_count()
 
-myHost = LinuxHost(totalMemory, totalCores)
+myHost = LinuxHost()
 
 print(myHost)
