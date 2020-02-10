f91ebb4862c631e0a52fedc1ed84ebe69aeeb919
Igor Braginsky <ibragins@redhat.com>
2020-02-10 21:56:16 +0200

    Implemented running CLI to get JSON describing VM. JSON was passed to constructor and respective object is created.

--- RunHWA.py
+++ RunHWA.py
@@ -6,24 +6,33 @@ from utils.utils import *
 def create_parser():
     cli_parser = argparse.ArgumentParser()
     cli_parser.add_argument('-n', '--name', help="VM name to get JSON from", dest='name', action='store')
-    cli_parser.add_argument('-c', help="Count amount of cores", dest='string', action='store')
+    cli_parser.add_argument('-c', help="Count amount of cores", dest='cpu_count', action='store')
     return cli_parser
 
 
 if __name__ == "__main__":
     parser = create_parser()
     args = parser.parse_args()
+    cli_line = 'export KUBECONFIG=/home/igor/oc4/working/auth/kubeconfig; ' \
+               # 'oc login -u kubeadmin -p $(cat ~/oc4/working/auth/kubeadmin-password);'
+
+    # exec_cli(cli_line)
 
     print(args)
     if args.name:
-        pass
+        cli_line += 'oc get vmi ' + args.name + ' -o json'
     else:
         exit_method(-1, "No VM name was provided")
+
+    cli_result = exec_cli(cli_line)
+    # cli_result = clear_string(cli_result, '\n')
     # if args.c:
     #     print(args.c)
 
-    j = openjson("vm_new.json")
-    myHost = LinuxHost(j)
+    # j = openjson("vm_new.json")
+    json_obj = convert_to_json(cli_result)
+    print(json_obj)
+    myHost = LinuxHost(json_obj)
 
     print(myHost)
     # print(myHost.volumes_list)
--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,6 @@
 import json
+import os
+import re
 
 
 def openfile(filename):
@@ -6,13 +8,34 @@ def openfile(filename):
     return f
 
 
-def openjson(filename):
+def open_json(filename):
     with open(filename) as json_file:
-        return json.load(json_file)
+        try:
+            return json.load(json_file)
+        except:
+            return None
     # j = json.loads(openfile(filename))
     # return j
+
+
+def convert_to_json(json_obj):
+    try:
+        return json.loads(json_obj)
+    except:
+        exit_method(-1, 'Json not loaded!')
+        return None
 
 
 def exit_method(code, msg=''):
     print(msg)
     exit(code)
+
+
+def exec_cli(cli):
+    stream = os.popen(cli)
+    return stream.read()
+
+
+def clear_string(string, mask):
+    string = re.sub(mask, '', string)
+    return string
